8b43d7922969d79e1553e9c8f7a7c2fec5b169cb
Danniel Vargas Roque <dannielvroque@gmail.com>
2025-03-21 16:11:52 -0300

    ajuste no destroy de todos os recursos

--- _backend.tf
+++ _backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "s3-tfsate-danniel-unique123"  # Nome exclusivo do bucket
+    bucket = "s3-tfsate-danniel-unique123"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-1"  # A região pode ser qualquer uma
     encrypt = true
   }
 }
