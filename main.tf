007cce04d9e069f5da9b32e29529569df313b9d9
Danniel Vargas Roque <dannielvroque@gmail.com>
2025-03-21 14:54:59 -0300

    teste usando 2 providers

--- main.tf
+++ main.tf
@@ -1,12 +1,33 @@
-provider "aws" {
-  region = "us-east-1" # Substitua pela sua região
+variable "regions" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-12345678"
+    sa-east-1 = "ami-87654321"
+  }
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-02e136e904f3da870" # Substitua por uma AMI válida da sua região
-  instance_type = "t2.micro"
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
 
-  tags = {
-    Name = "Terraform-EC2"
+provider "aws" {
+  alias  = "sa-east-1"
+  region = "sa-east-1"
+}
+
+# Mapeamento de alias dos providers
+locals {
+  providers_map = {
+    us-east-1 = aws.us-east-1
+    sa-east-1 = aws.sa-east-1
   }
+}
+
+# Criar instâncias dinamicamente em todas as regiões definidas
+resource "aws_instance" "instances" {
+  for_each      = var.regions
+  provider      = local.providers_map[each.key] # Resolvendo o provider dinamicamente
+  ami           = each.value
+  instance_type = "t2.micro"
 }
