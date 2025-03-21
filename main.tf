44385fa62b4ee1123390e8cb30c6fe1b151e3253
Danniel Vargas Roque <dannielvroque@gmail.com>
2025-03-21 15:52:23 -0300

    usando bucket

--- main.tf
+++ main.tf
@@ -151,3 +151,11 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 
+terraform {
+  backend "s3" {
+    bucket = "s3-tfsate-danniel-unique123"  # Nome exclusivo do bucket
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
+}
